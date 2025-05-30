e142c441c92a047cc31c0a76dda04e9b802b51dd
xlocaste <irfandi.di122@gmail.com>
2025-05-31 01:51:08 +0700

    feat:admin panel store pemesanan

--- app/Http/Controllers/PemesananController.php
+++ app/Http/Controllers/PemesananController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Pemesanan\StoreRequest;
+use App\Models\Kontrak;
 use App\Models\Pemesanan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -18,6 +20,25 @@ class PemesananController extends Controller
             'auth' => [
                 'user' => Auth::user(),
             ],
+        ]);
+    }
+
+    public function store(StoreRequest $request)
+    {
+        Pemesanan::create([
+            'kontrak_id' => $request->kontrak_id,
+            'nama_barang' => $request->nama_barang,
+            'jumlah' => $request->jumlah,
+            'satuan' => $request->satuan,
+        ]);
+
+        return redirect()->route('pemesanan.index');
+    }
+
+    public function create()
+    {
+        return Inertia::render('Pemesanan/Add', [
+            'kontrak' => Kontrak::all(),
         ]);
     }
 }
