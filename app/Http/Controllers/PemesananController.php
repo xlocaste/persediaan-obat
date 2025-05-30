83299f45175cd680e8ecab78cff2776113802cf8
xlocaste <irfandi.di122@gmail.com>
2025-05-31 01:55:46 +0700

    feat:admin panel update pemesanan

--- app/Http/Controllers/PemesananController.php
+++ app/Http/Controllers/PemesananController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Pemesanan\StoreRequest;
+use App\Http\Requests\Pemesanan\UpdateRequest;
 use App\Models\Kontrak;
 use App\Models\Pemesanan;
 use Illuminate\Http\Request;
@@ -33,6 +34,26 @@ class PemesananController extends Controller
         ]);
 
         return redirect()->route('pemesanan.index');
+    }
+
+    public function update(UpdateRequest $request, Pemesanan $pemesanan)
+    {
+        $pemesanan->update([
+            'kontrak_id' => $request->kontrak_id,
+            'nama_barang' => $request->nama_barang,
+            'jumlah' => $request->jumlah,
+            'satuan' => $request->satuan,
+        ]);
+
+        return redirect()->route('pemesanan.index');
+    }
+
+    public function edit(Pemesanan $pemesanan)
+    {
+        return Inertia::render('Pemesanan/Update', [
+            'pemesanan' => $pemesanan,
+            'kontrak' => Kontrak::all(),
+        ]);
     }
 
     public function create()
---
+++ app/Http/Requests/Pemesanan/UpdateRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Pemesanan;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'kontrak_id' => ['required', 'exists:kontrak,id'],
+            'nama_barang' => ['required', 'string', 'max:255'],
+            'jumlah' => ['required', 'integer', 'min:1'],
+            'satuan' => ['required', 'string', 'max:100'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'kontrak_id.required' => 'Pilih kontrak terlebih dahulu.',
+            'kontrak_id.exists' => 'Kontrak yang dipilih tidak valid.',
+
+            'nama_barang.required' => 'Nama barang wajib diisi.',
+            'nama_barang.string' => 'Nama barang harus berupa teks.',
+            'nama_barang.max' => 'Nama barang maksimal 255 karakter.',
+
+            'jumlah.required' => 'Jumlah wajib diisi.',
+            'jumlah.integer' => 'Jumlah harus berupa angka bulat.',
+            'jumlah.min' => 'Jumlah minimal 1.',
+
+            'satuan.required' => 'Satuan wajib diisi.',
+            'satuan.string' => 'Satuan harus berupa teks.',
+            'satuan.max' => 'Satuan maksimal 100 karakter.',
+        ];
+    }
+}
