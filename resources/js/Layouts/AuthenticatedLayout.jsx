a7139d5390d6e132ef09373eff35a1a288ae8724
Manot22 <arimanut41@gmail.com>
2025-05-31 16:16:30 +0700

    style:admin panel authentication layout arrow drop down user

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,6 +1,6 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link, usePage } from '@inertiajs/react';
-import { useState } from 'react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link, usePage } from "@inertiajs/react";
+import { useState } from "react";
 
 export default function AuthenticatedLayout({ header, children }) {
     const user = usePage().props.auth.user;
@@ -18,43 +18,43 @@ export default function AuthenticatedLayout({ header, children }) {
 
                 <div className="p-4 space-y-2 flex flex-col">
                     <Link
-                        href={route('distributor.index')}
-                        active={route().current('distributor.index')}
+                        href={route("distributor.index")}
+                        active={route().current("distributor.index")}
                         className="p-2 rounded-lg hover:bg-white hover:text-indigo-700 focus:bg-white focus:text-indigo-700 transition duration-300 ease-in-out"
                     >
                         Distributor
                     </Link>
                     <Link
-                        href={route('kontrak.index')}
-                        active={route().current('kontrak.index')}
+                        href={route("kontrak.index")}
+                        active={route().current("kontrak.index")}
                         className="p-2 rounded-lg hover:bg-white hover:text-indigo-700 focus:bg-white focus:text-indigo-700 transition duration-300 ease-in-out"
                     >
                         Kontrak
                     </Link>
                     <Link
-                        href={route('pemesanan.index')}
-                        active={route().current('pemesanan.index')}
+                        href={route("pemesanan.index")}
+                        active={route().current("pemesanan.index")}
                         className="p-2 rounded-lg hover:bg-white hover:text-indigo-700 focus:bg-white focus:text-indigo-700 transition duration-300 ease-in-out"
                     >
                         Pemesanan
                     </Link>
                     <Link
-                        href={route('pengiriman.index')}
-                        active={route().current('pengiriman.index')}
+                        href={route("pengiriman.index")}
+                        active={route().current("pengiriman.index")}
                         className="p-2 rounded-lg hover:bg-white hover:text-indigo-700 focus:bg-white focus:text-indigo-700 transition duration-300 ease-in-out"
                     >
                         Pengiriman
                     </Link>
                     <Link
-                        href={route('penerima.index')}
-                        active={route().current('penerima.index')}
+                        href={route("penerima.index")}
+                        active={route().current("penerima.index")}
                         className="p-2 rounded-lg hover:bg-white hover:text-indigo-700 focus:bg-white focus:text-indigo-700 transition duration-300 ease-in-out"
                     >
                         Penerima
                     </Link>
                     <Link
-                        href={route('bukti-penerimaan.index')}
-                        active={route().current('bukti-penerimaan.index')}
+                        href={route("bukti-penerimaan.index")}
+                        active={route().current("bukti-penerimaan.index")}
                         className="p-2 rounded-lg hover:bg-white hover:text-indigo-700 focus:bg-white focus:text-indigo-700 transition duration-300 ease-in-out"
                     >
                         Bukti Penerimaan
@@ -62,13 +62,34 @@ export default function AuthenticatedLayout({ header, children }) {
 
                     <button
                         onClick={() => setShowDropdown(!showDropdown)}
-                        className="text-left p-2 rounded-lg hover:bg-white hover:text-indigo-700 focus:bg-white focus:text-indigo-700 transition duration-300 ease-in-out"
+                        className="flex items-center justify-between w-full p-2 rounded-lg hover:bg-white hover:text-indigo-700 focus:bg-white focus:text-indigo-700 transition duration-300 ease-in-out"
                     >
                         {user.name}
+                        <svg
+                            className={`w-4 h-4 ml-2 transition-transform duration-300 ease-in-out ${
+                                showDropdown ? "rotate-180" : "rotate-0"
+                            }`}
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M19 9l-7 7-7-7"
+                            />
+                        </svg>
                     </button>
 
                     {showDropdown && (
-                        <Link className="text-left p-2 rounded-lg hover:bg-white hover:text-indigo-700 focus:bg-white focus:text-indigo-700 transition duration-300 ease-in-out" href={route('logout')} method="post" as="button">
+                        <Link
+                            className="text-left p-2 rounded-lg hover:bg-white hover:text-indigo-700 focus:bg-white focus:text-indigo-700 transition duration-300 ease-in-out"
+                            href={route("logout")}
+                            method="post"
+                            as="button"
+                        >
                             <p>Log Out</p>
                         </Link>
                     )}
