096635ad274ae124a2aaf3e7ed5ca863b52d8f5a
xlocaste <irfandi.di122@gmail.com>
2025-06-13 19:56:25 +0700

    fix:welcome route /

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,12 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
 Route::get('/dashboard', function () {
